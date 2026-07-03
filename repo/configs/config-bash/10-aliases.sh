e92d78e3c2f7dd14057a6eaf552e0384f4c178b5
HM. Wassim <habibimedwassim@gmail.com>
2026-07-03 03:51:28 +0100

    chore: split bashrc.d snippets into files
    - Delete monolithic 99-config-bash.sh
    - Split into readline, aliases, starship
    - Update config-bash.yaml with three entries
    - Enable numeric ordering in bashrc.d

--- repo/configs/config-bash/10-aliases.sh
+++ repo/configs/config-bash/10-aliases.sh
@@ -1,10 +1,6 @@
-bind "set completion-ignore-case on"
-
 alias ls='eza -al --icons --color=always --group-directories-first'
 alias la='eza -a  --icons --color=always --group-directories-first'
 alias ll='eza -l  --icons --color=always --group-directories-first'
 alias lt='eza -aT --icons --color=always --group-directories-first'
 
 alias cat='batcat --style=plain --pager=never'
-
-eval "$(starship init bash)"
