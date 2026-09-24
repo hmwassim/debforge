92857fd20455f696e2c4c83cc4eac423f26eec9b
HM. Wassim <habibimedwassim@gmail.com>
2026-09-24 23:41:24 +0100

    fix: update configs

--- repo/configs/config-bash/10-aliases.sh
+++ repo/configs/config-bash/10-aliases.sh
@@ -1,6 +1,6 @@
-alias ls='eza -al --icons --color=always --group-directories-first'
-alias la='eza -a  --icons --color=always --group-directories-first'
-alias ll='eza -l  --icons --color=always --group-directories-first'
-alias lt='eza -aT --icons --color=always --group-directories-first'
+alias ls='eza -al --icons --color=auto --group-directories-first'
+alias la='eza -a  --icons --color=auto --group-directories-first'
+alias ll='eza -l  --icons --color=auto --group-directories-first'
+alias lt='eza -aT --icons --color=auto --group-directories-first'
 
 alias cat='batcat --style=plain --pager=never'
